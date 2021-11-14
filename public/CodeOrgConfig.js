291bf90c6e7a50fb1069a50b16d1864636d1f927
Exotek Technologies <76646708+RobotEngine@users.noreply.github.com>
2021-11-14 00:40:03 -0600

    Delete CodeOrgConfig.js

--- public/CodeOrgConfig.js
+++
@@ -1,8 +0,0 @@
-var LoadScripts = { 
-  Feed: "https://serverphotop.anthonybeckett.repl.co/Feed.js",
-  BridgeConnect: "https://serverphotop.anthonybeckett.repl.co/BridgeConnect.js",
-  PostRender: "https://serverphotop.anthonybeckett.repl.co/PostRender.js",
-  ImageUpload: "https://serverphotop.anthonybeckett.repl.co/FirebaseMedia.js",
-  AdManager: "https://serverphotop.anthonybeckett.repl.co/AManager.js",
-  Settings: "https://serverphotop.anthonybeckett.repl.co/Settings.js"
-};
